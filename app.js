1e01508a9091c344f85eaa970da2d944f14ab0bb
JonMoore <jonmoore9000@gmail.com>
2021-08-06 13:22:22 -0600

    first

---
+++ app.js
@@ -0,0 +1,23 @@
+
+$(document).ready(function() {
+  const base ='https://public-api.tracker.gg/v2/splitgate/standard/profile/psn/B7REC';
+  $('.search').click(function() {
+    $.ajax({
+      url: base,
+      //dataType: 'jsonp',
+      headers: {
+        'TRN-Api-Key': '7eb1cf3e-241f-4f6f-87a4-00e704d455e3',
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Headers": "*",
+        'Access-Control-Allow-Origin': 'https://tracker.gg'
+      },
+      type: "GET",
+      success: function(result){
+        console.log(result)
+      },
+      error: function(error) {
+        console.log('Error ${error}')
+      }
+    })
+  })
+  })
